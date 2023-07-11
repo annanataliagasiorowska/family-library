e912dd35ec34a46bc41fc58e370d603aa7be3ce6
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-07-11 17:32:10 +0200

    initial commit

---
+++ src/main/java/pl/sasieczno/familyLibrary/FamilyLibraryApplication.java
@@ -0,0 +1,13 @@
+package pl.sasieczno.familyLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sasieczno/familyLibrary/FamilyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sasieczno.familyLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
